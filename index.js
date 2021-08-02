249a794f12af7848a6661dc1a7c7c5064f30bf22
Josh Cowan <cowanjzc@gmail.com>
2021-08-01 20:19:17 -0400

    3.14

--- index.js
+++ index.js
@@ -20,30 +20,33 @@ app.get("/api/persons", (request, response) => {
 });
 
 app.get("/info", (request, response) => {
-  response.send(
-    `<p>Phonebook has information for ${
-      people.length
-    } people.</p><p>${Date()}</p>`
-  );
+  Instance.find({}).then((result) => {
+    response.send(
+      `<p>Phonebook has information for ${
+        result.length
+      } people.</p><p>${Date()}</p>`
+    );
+  });
 });
 
 app.get("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const person = people.find((p) => p.id === id);
-  if (person) {
-    response.json(person);
-  } else {
-    response.send(
-      "<h1>404</h1><p>Error!  That note id could not be found in the dataset.</p>"
-    );
-  }
+  const id = request.params.id;
+  Instance.find({ id: id }).then((result) => {
+    if (result.length === 1) {
+      response.json(result[0]);
+    } else {
+      response.send(
+        "<h1>404</h1><p>Error!  That note id could not be found in the dataset.</p>"
+      );
+    }
+  });
 });
 
 app.delete("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  person = people.filter((p) => p.id !== id);
-
-  response.status(204).end();
+  const id = request.params.id;
+  Instance.findByIdAndRemove(id).then((result) => {
+    response.status(204).end();
+  });
 });
 
 app.post("/api/persons", (request, response) => {
@@ -56,23 +59,11 @@ app.post("/api/persons", (request, response) => {
     return response.status(400).json({ error: "Phone Number is missing." });
   }
 
-  if (
-    people.filter((p) => {
-      p.name === body.name;
-    }).length > 0
-  ) {
-    return response
-      .status(400)
-      .json({ error: "That person already has an entry in the phonebook." });
-  }
-
-  const new_number = {
-    id: parseInt(Math.random(0, 100000) * 100000),
-    name: body.name,
-    number: body.number,
-  };
-  people = people.concat(new_number);
-  response.json(new_number);
+  const instance = new Instance({ name: body.name, number: body.number });
+  instance.save().then((result) => {
+    console.log("Phone Number Saved.");
+    response.json(result);
+  });
 });
 
 const PORT = process.env.PORT || 3001;
